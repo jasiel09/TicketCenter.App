21c9f40cfedf86150ab6fba6af6b953a9a59bb18
jcascante <jcascante@SUCR-P021>
2023-08-03 15:52:46 -0600

    Agregar archivos de proyecto.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketCenter;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace TicketCenter;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
