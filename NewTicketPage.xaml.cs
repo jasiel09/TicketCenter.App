3cfb7f34eae974e12ce7ea6022c87f25d72ee49e
victor <vicsalza@gmail.com>
2023-08-16 19:34:24 -0600

    New Ticket

--- NewTicketPage.xaml.cs
+++ NewTicketPage.xaml.cs
@@ -5,5 +5,24 @@ public partial class NewTicketPage : ContentPage
 	public NewTicketPage()
 	{
 		InitializeComponent();
-	}
+        Type.SelectedIndex = 0;
+        Category.SelectedIndex = 0;
+        Priority.SelectedIndex = 0;
+        Owner.SelectedIndex = 0;
+        Assigned.SelectedIndex = 0;
+    }
+
+    private async void SelectFileButton_Clicked(object sender, EventArgs e)
+    {
+        var fileResult = await FilePicker.PickAsync(new PickOptions
+        {
+            FileTypes = FilePickerFileType.Images,
+            PickerTitle = "Selected file:"
+        });
+
+        if (fileResult != null)
+        {
+            selectedFileLabel.Text = $"Archivo Seleccionado: {fileResult.FileName}";
+        }
+    }
 }
